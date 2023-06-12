65208fcf634b160a04bac042b951ad7f297250f8
firas-rouine <rouine.firas@gmail.com>
2023-06-12 17:41:15 +0100

    commit

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 
 from flask_app import app
 # ! DON'T Forget to import ALL Controllers Here
-from flask_app.controllers import logregss
+from flask_app.controllers import users,institutions_controllers
 
 if __name__ == '__main__':
     app.run(debug =True)
